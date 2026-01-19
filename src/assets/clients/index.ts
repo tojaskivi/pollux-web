c580a850387d93b213cf10c4e4499630602ac2f8
tommi <tommiojaskivi@gmail.com>
2026-01-19 11:04:29 +0100

    WIP - finalizing UI, adding images etc

---
+++ src/assets/clients/index.ts
@@ -0,0 +1,3 @@
+export { default as stockholmExergi } from "./stockholm_exergi.png";
+export { default as stockholmsHamnar } from "./stockholms_hamnar.png";
+export { default as zanderIngestrom } from "./zander_ingestrom.png";
---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+export { default as heroImage } from "./hero.webp";
+export { default as aboutImage } from "./about.webp";
+export { default as serviceImage } from "./service.webp";
+export { default as service1Image } from "./service_1.webp";
+export { default as service2Image } from "./service_2.webp";
+export { default as service3Image } from "./service_3.webp";
