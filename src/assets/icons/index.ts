e6b6f72e5467d2754b7dcd7138c9a6da35eabbc8
tommi <tommiojaskivi@gmail.com>
2026-01-19 12:08:06 +0100

    Add close and menu icons, enhance header and footer responsiveness, and update hero title

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as logo } from "./logo.svg";
 export { default as logomark } from "./logomark.svg";
+export { default as menuIcon } from "./menu.png";
+export { default as closeIcon } from "./close.svg";
